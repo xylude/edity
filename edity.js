09604b49da1eabe7dbf8e3f0e44118a245dd3c09
Jason Crider <xylude@gmail.com>
2013-01-12 09:40:44 -0500

    dynamic adding of buttons using the passed in object

--- edity.js
+++ edity.js
@@ -2,15 +2,46 @@
     $.fn.edity = function(opts) {
         this.each(function(i,v){
             //get css values of original textarea
+            var textarea = $(this);
             var original_val = $(this).val();
             var width = $(this).width();
             var height = $(this).height();
             var id = "editor_"+i;
+            var buttons = ['bold','italic'];
+            if(opts) {
+                if(opts.buttons) {
+                    buttons = opts.buttons;
+                }                
+            }
+            
+            btn_html = '';
+            for(i=0;i<buttons.length;i++) {
+                btn_html+='<li class="'+buttons[i]+'">'+buttons[i]+'</li>';
+            }
+            
+            var action_bar = "<ul class='action_bar'>"+btn_html+"</ul>";
+            var iframe = "<iframe id='"+id+"' style='width:"+width+"px;height:"+height+"px;'></iframe>";
             
             $(this).hide();
-            $(this).before("<iframe id='"+id+"' style='width:"+width+"px;height:"+height+"px;'></iframe>");
-            document.getElementById(id).contentWindow.document.write(original_val);
-            document.getElementById(id).contentDocument.designMode = "on";
+            $(this).before(action_bar+iframe);
+            var win = document.getElementById(id).contentWindow;
+            var doc = document.getElementById(id).contentDocument;
+            
+            win.document.write(original_val);
+            doc.designMode = "on";
+            
+            var iframeBody=$('#'+id).contents().find('body');
+            $(iframeBody).on('keyup',function() {
+                var txt = $(this).html();
+                textarea.val(txt);
+                console.log(textarea.val());
+            });
+            
+            $('.action_bar').on('click','li',function(){
+                var cmd = $(this).attr('class');
+                win.focus();
+                win.document.execCommand(cmd,false,'');
+            });
         });
     }
 })(jQuery);
