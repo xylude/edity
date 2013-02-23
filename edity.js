51c2073c9352211648cfe4de6055468bed72568e
Jason Crider <jason.crider@eproximiti.com>
2013-02-23 14:05:36 -0500

    added font sizing and url insert. Theres a bug with url inserting atm though

--- edity.js
+++ edity.js
@@ -22,6 +22,8 @@
                 var id = "edity-the-editor_"+i;
                 var buttons = ['bold','italic'];
                 var image_path = 'images/';
+                var dropdown_buttons = '';
+                
                 if(opts) {
                     if(opts.buttons) {
                         buttons = opts.buttons;
@@ -31,9 +33,23 @@
                     }
                 }
             
-                btn_html = '';
+                var btn_html = '';
+                var btn_prompt = '';
                 for(i=0;i<buttons.length;i++) {
-                    btn_html+='<li class="edity-the-editor_'+buttons[i]+'"><img src="'+image_path+buttons[i]+'.png" /></li>';
+                    btn_prompt = '';
+                    console.log('adding button '+buttons[i]);
+                    if(buttons[i]=='fontSize') {  
+                        dropdown_buttons = '<ul>';
+                        console.log('adding fontSize button');
+                        for(var ii=1;ii<8;ii++) {
+                            dropdown_buttons+='<li class="edity-the-editor_fontSize" data-val="'+ii+'">'+ii+'</li>';
+                        }
+                        dropdown_buttons+='</ul>';
+                    } else if(buttons[i]=='createLink') {
+                        btn_prompt = ' data-prompt="Enter URL"';
+                    }
+                    btn_html+='<li'+btn_prompt+' class="edity-the-editor_'+buttons[i]+'" style="background-image:url('+image_path+buttons[i]+'.png);">'+dropdown_buttons+'</li>';
+                    dropdown_buttons = '';
                 }
             
                 var action_bar = "<ul class='edity-the-editor_action_bar'>"+btn_html+"</ul>";
@@ -61,15 +77,28 @@
                 });
             
                 $('.edity-the-editor_action_bar').on('click','li',function(){
-                    var cmd = $(this).attr('class').replace('edity-the-editor_','');
-                    win.focus();
-                    win.document.execCommand(cmd,false,'');
-                    //get current text:
-                    console.log('command '+cmd);
-                    textarea.val(iframeBody.html());
-                    if(opts) {
-                        if(opts.onchange) {
-                            opts.onchange(textarea,iframeBody.html());
+                    if($(this).children('ul').length==0) {
+                        var cmd = $(this).attr('class').replace('edity-the-editor_','');
+                        var value = ($(this).attr('data-val'))?$(this).attr('data-val'):'';
+                        if($(this).attr('data-prompt')) {
+                            value = prompt($(this).attr('data-prompt'));
+                        }
+                        win.focus();
+                        win.document.execCommand(cmd,false,value);
+                        //get current text:
+                        console.log('command '+cmd);
+                        textarea.val(iframeBody.html());
+                        if(opts) {
+                            if(opts.onchange) {
+                                opts.onchange(textarea,iframeBody.html());
+                            }
+                        }
+                    } else {
+                        //display dropdown:
+                        if($(this).children('ul').is(':visible')) {
+                            $(this).children('ul').hide()   
+                        } else {
+                            $(this).children('ul').show();
                         }
                     }
                 });
