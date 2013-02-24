2b25385209439317a1811c020684a844d12a825f
Jason Crider <xylude@gmail.com>
2013-02-24 17:14:09 -0500

    Believe that I have fixed most of the issues at hand. Font size change now works. Adding Links now works. When .reinitialize() is called all event listeners are stripped from the buttons and re-attached.

--- edity.js
+++ edity.js
@@ -2,12 +2,24 @@
     $.fn.edity = function(opts) {
         
         this.reinitialize = function() {
-            $('.edity-the-editor_wrapper').remove();
+            this.remove_editors();
             this.run();
         }
+        
+        this.remove_editors = function() {
+            $('.edity-the-editor_wrapper').remove();
+            $(this).each(function(){
+                $('body').off('click','#edity-the-editor_action_bar_'+$(this).data('edity-id')+' li');
+                console.log('removed click event for #edity-the-editor_action_bar_'+$(this).data('edity-id')+' li')
+                $(this).data('edity-id',null);
+                $(this).data('listener-attached',null);
+            });
+        }
+        
         this.run = function() {
             $(this).each(function(i,v){
                 //get css values of original textarea
+                console.log($(this).data());
                 var textarea = $(this);
                 var original_val = $(this).val();
                 var width = $(this).width();
@@ -20,9 +32,12 @@
                 }
                 
                 var id = "edity-the-editor_"+i;
+                var bar_id = 'edity-the-editor_action_bar_'+i
                 var buttons = ['bold','italic'];
                 var image_path = 'images/';
                 var dropdown_buttons = '';
+                
+                textarea.data('edity-id',i);
                 
                 if(opts) {
                     if(opts.buttons) {
@@ -37,10 +52,8 @@
                 var btn_prompt = '';
                 for(i=0;i<buttons.length;i++) {
                     btn_prompt = '';
-                    console.log('adding button '+buttons[i]);
                     if(buttons[i]=='fontSize') {  
                         dropdown_buttons = '<ul>';
-                        console.log('adding fontSize button');
                         for(var ii=1;ii<8;ii++) {
                             dropdown_buttons+='<li class="edity-the-editor_fontSize" data-val="'+ii+'">'+ii+'</li>';
                         }
@@ -52,7 +65,7 @@
                     dropdown_buttons = '';
                 }
             
-                var action_bar = "<ul class='edity-the-editor_action_bar'>"+btn_html+"</ul>";
+                var action_bar = "<ul class='edity-the-editor_action_bar' id='"+bar_id+"'>"+btn_html+"</ul>";
                 var iframe = "<iframe frameborder='0' scrolling='yes' id='"+id+"'></iframe>";
             
                 $(this).hide();
@@ -75,33 +88,40 @@
                         }
                     }
                 });
-            
-                $('.edity-the-editor_action_bar').on('click','li',function(){
-                    if($(this).children('ul').length==0) {
-                        var cmd = $(this).attr('class').replace('edity-the-editor_','');
-                        var value = ($(this).attr('data-val'))?$(this).attr('data-val'):'';
-                        if($(this).attr('data-prompt')) {
-                            value = prompt($(this).attr('data-prompt'));
-                        }
-                        win.focus();
-                        win.document.execCommand(cmd,false,value);
-                        //get current text:
-                        console.log('command '+cmd);
-                        textarea.val(iframeBody.html());
-                        if(opts) {
-                            if(opts.onchange) {
-                                opts.onchange(textarea,iframeBody.html());
+                
+                
+                if(!textarea.data('listener-attached')) {
+                    textarea.data('listener-attached', true);
+                    console.log('attaching listeners');
+                    $('body').on('click','#'+bar_id+' li',function(){
+                        if($(this).children('ul').length==0) {
+                            var c_id = $(this).parents('.edity-the-editor_action_bar').attr('id').replace('edity-the-editor_action_bar_','');
+                            var iframe = document.getElementById('edity-the-editor_'+c_id).contentWindow;
+                            var cmd = $(this).attr('class').replace('edity-the-editor_','');
+                            var value = ($(this).attr('data-val'))?$(this).attr('data-val'):'';
+                            if($(this).attr('data-prompt')) {
+                                value = prompt($(this).attr('data-prompt'));
+                            }
+                            iframe.focus();
+                            iframe.document.execCommand(cmd,false,value);
+                            console.log('issued command '+cmd);
+                            //get current text:
+                            textarea.val(iframeBody.html());
+                            if(opts) {
+                                if(opts.onchange) {
+                                    opts.onchange(textarea,iframeBody.html());
+                                }
+                            }
+                        } else {
+                            //display dropdown:
+                            if($(this).children('ul').is(':visible')) {
+                                $(this).children('ul').hide()   
+                            } else {
+                                $(this).children('ul').show();
                             }
                         }
-                    } else {
-                        //display dropdown:
-                        if($(this).children('ul').is(':visible')) {
-                            $(this).children('ul').hide()   
-                        } else {
-                            $(this).children('ul').show();
-                        }
-                    }
-                });
+                    });
+                }
             });
         }
         this.run();
